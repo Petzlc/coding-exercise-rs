6deaffc346b6ab66302def5d76d5660144abd61a
Petzlc <chlodwig.petzl@hotmail.com>
2024-11-21 14:40:28 +0100

    Instal dialog and add to components

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+'use client';
+
+import {
+  DateSelectArg,
+  EventApi,
+  EventClickArg,
+  formatDate,
+} from '@fullcalendar/core/index.js';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import FullCalendar from '@fullcalendar/react';
+import React, { useEffect, useState } from 'react';
 
 export default function Calendar() {
-  return (
-    <div>Calendar</div>
-  )
+  const [currentEvents, setCurrentEvents] = useState<EventApi[]>([]);
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  const [newEventTitle, setNewEventTitle] = useState<string>('');
+  const [selectedDate, setSelectedDate] = useState<DateSelectArg | null>(null);
+
+  const handleDateClick = (selected: DateSelectArg) => {
+    setSelectedDate(selected);
+    setIsDialogOpen(true);
+  };
+
+  return <div>Calendar</div>;
 }
